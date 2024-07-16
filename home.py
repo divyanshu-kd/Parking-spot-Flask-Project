543431aa4fc2c3271acafdd9cfe4b19101ae1314
Divyanshu <divyanshuiit02@gmail.com>
2024-07-16 17:22:23 +0530

    adding react templates

--- home.py
+++ home.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return '<h1>Hi!</h1>'
+    return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
