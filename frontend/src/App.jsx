488fa3df0e47aa52159cb532cc4f1f490e6d37f3
Divyanshu <divyanshuiit02@gmail.com>
2024-07-18 11:11:44 +0530

    creating login page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,20 @@
 function App() {
 
   return (
-    <p className="text-red-300">hi</p>
+    <div className="flex flex-col justify-center items-center h-screen">
+      <div className="flex flex-col justify-center items-center border-2 rounded-lg p-4">
+        <div>Login</div>
+        <div>
+          <i class='bx bxs-user-circle'></i>
+          <input type="text" placeholder="username" className="outline-none border rounded-lg p-2 mb-1"/>
+        </div>
+        <div className="">
+          <i class='bx bxs-lock'></i>
+          <input type="password" placeholder="password" className="outline-none border rounded-lg p-2 mt-1"/>
+        </div>
+        <div>creat new <a href="/register" >account</a></div>
+      </div>
+    </div>
   )
 }
 
